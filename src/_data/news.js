134c573552ba281e6fec4b89c57c403f1280fd74
Kristi Achilleos <krispy2009@gmail.com>
2021-01-31 12:13:48 +0200

    rename api key and fix typo

--- src/_data/news.js
+++ src/_data/news.js
@@ -4,10 +4,10 @@ require("dotenv").config();
 module.exports = async function () {
   try {
     const response = await axios.get(
-      `http://newsapi.org/v2/everything?q=tesla&from=2020-12-31&sortBy=publishedAt&apiKey=${process.env.API_KEY}&pageSize=5`
+      `http://newsapi.org/v2/everything?q=tesla&from=2020-12-31&sortBy=publishedAt&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`
     );
     return response.data;
   } catch (error) {
-    console.error(errror);
+    console.error(error);
   }
 };
